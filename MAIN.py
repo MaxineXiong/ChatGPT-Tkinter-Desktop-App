741a61070ef7da87b9950ebaf7f77ab1ed2c9653
MaxineXiong <maxinexiong2@gmail.com>
2024-06-22 04:56:48 +1000

    venv folder removed

---
+++ MAIN.py
@@ -0,0 +1,318 @@
+from tkinter import *
+import customtkinter
+from tkinter import messagebox
+# Import the ChatGPTBot class from bot.py
+from bot import ChatGPTBot
+from datetime import datetime
+
+
+# Define the class for the chat app
+class TkinterApp:
+    # The initializer method defines the GUI of the application window
+    def __init__(self):
+        # Initialize variables for the bot, KEY and bot_message
+        self.bot = None
+        self.KEY = ''
+        self.MODEL = ''
+        self.bot_message = ''
+        self.key_part_displayed = False
+
+
+        # Initiate the App
+        # Create a new window using the CTk class from the customtkinter module
+        self.window = customtkinter.CTk(fg_color = '#2B2D31')
+        # Set the title of the window
+        self.window.title('ChatGPT Tkinter App')
+        # Set the size of the window
+        self.window.geometry('720x615')
+        # Set the window icon
+        self.window.iconbitmap('./icons/chat.ico')
+        # Set dark mode
+        customtkinter.set_appearance_mode('dark')
+        # Set default color theme
+        customtkinter.set_default_color_theme('blue')
+
+
+        # Create a frame for the chat window and its attached scrollbar
+        frame_chatwin = customtkinter.CTkFrame(master = self.window, fg_color = '#2B2D31')  # fg_color: color inside frame
+        frame_chatwin.pack(pady = 40)  # pady: padding on y axis
+
+        # Create a chat window as a Text widget within the frame
+        # Set the background color (i.e. bg), width, border width (i.e. bd), font color (i.e. fg), relief style, cursor color (i.e. insertbackground),
+        # text wrapping (i.e. wrap), and selection background color
+        self.chat_window = Text(master = frame_chatwin,
+                                bg = '#313338',
+                                width = 65,
+                                height = 20,
+                                bd = 1,
+                                fg = '#BABCBE',
+                                relief = 'flat',
+                                insertbackground = '#6699FF',
+                                wrap = WORD,
+                                selectbackground = '#5865f2')  # wrap = WORD: When moving to the next line, the entire word will be wrapped to the next line instead of being split in the middle
+                                                               # selectbackground: Defines the background color of the text when it is selected
+        self.chat_window.grid(row = 0, column = 0)
+
+        # Create a vertical scroll bar for the chat window
+        scrollbar_chatwin = customtkinter.CTkScrollbar(master = frame_chatwin,
+                                                       orientation = 'vertical',
+                                                       command = self.chat_window.yview)
+        scrollbar_chatwin.grid(row = 0, column = 1, sticky = N+S+W)
+
+        # Configure the chat window to use the scrollbar for vertical scrolling,
+        # set the font to 'sans-serif', and disable the manual entry into the chat window
+        self.chat_window.configure(yscrollcommand = scrollbar_chatwin.set,
+                                   font = ('sans-serif'),
+                                   state = DISABLED)
+
+
+        # Create an entry widget for the user to enter their message
+        self.user_msg_entry = customtkinter.CTkEntry(master = self.window,
+                                                     fg_color = '#313338',
+                                                     text_color = '#BABCBE',
+                                                     placeholder_text = "Type your message here and hit the 'Enter' key",
+                                                     width = 585,
+                                                     height = 50,
+                                                     border_width = 1)
+        self.user_msg_entry.pack(pady = 0)
+        # Bind the activity of pressing the Enter key to the send_text_message() function
+        self.user_msg_entry.bind('<Return>', self.send_text_message)
+
+
+        # Create a frame for 3 buttons
+        frame_buttons = customtkinter.CTkFrame(master = self.window, fg_color = '#2B2D31')
+        frame_buttons.pack(pady = 35)
+
+        # 'Update API Key' button
+        # Create a button to update the API key. When clicked, it calls the show_key_part() function
+        update_key_button = customtkinter.CTkButton(master = frame_buttons,
+                                                    text = 'Update API Key',
+                                                    fg_color = '#5865f2',
+                                                    hover_color = '#2133ee',
+                                                    height = 40,
+                                                    command = self.toggle_key_part)
+        update_key_button.grid(row = 0, column = 0, padx = 0)
+
+        # 'Listen To Message' button
+        # Create a button to play the bot's message in audio. When clicked, it calls the play_bot_message() function
+        listen_button = customtkinter.CTkButton(master = frame_buttons,
+                                                text = 'Listen To Message',
+                                                fg_color = '#5865f2',
+                                                hover_color = '#2133ee',
+                                                height = 40,
+                                                command = self.play_bot_message)
+        listen_button.grid(row = 0, column = 1, padx = 82)
+
+        # 'Clear History' button
+        # Create a button to clear the chat history while NOT reinitializing the chatbot. When clicked, it calls the clear_history() function
+        clear_button = customtkinter.CTkButton(master = frame_buttons,
+                                               text = 'Clear History',
+                                               fg_color = '#5865f2',
+                                               hover_color = '#2133ee',
+                                               height = 40,
+                                               command = self.clear_history)
+        clear_button.grid(row = 0, column = 2, padx = 0)
+
+
+        # Create a frame for API key part
+        # This frame will be shown or hidden by the update_key_button and save_button
+        self.frame_key = customtkinter.CTkFrame(master = self.window,
+                                                border_width = 1)
+
+        # Create an Entry widget for the user to enter their API key
+        self.key_entry = customtkinter.CTkEntry(master = self.frame_key,
+                                                placeholder_text = 'Enter you API key here',
+                                                fg_color = '#313338',
+                                                text_color = '#BABCBE',
+                                                width = 240,
+                                                height = 40,
+                                                border_width = 1)
+        self.key_entry.grid(row = 0, column = 0, padx = 20, pady = 20)
+
+        # Set initial option for the model selection combobox
+        self.model_var = customtkinter.StringVar(value = 'Select a GPT Model')
+        # Create a ComboBox widget for the user to select a GPT model
+        self.model_combobox = customtkinter.CTkComboBox(master = self.frame_key,
+                                                        values = ['Select a GPT Model', 'gpt-3.5-turbo', 'gpt-4o', 'gpt-4-turbo'],
+                                                        fg_color = '#313338',
+                                                        button_hover_color = '#5865f2',
+                                                        width = 175,
+                                                        height = 40,
+                                                        border_width = 1,
+                                                        variable = self.model_var)
+        self.model_combobox.grid(row = 0, column = 1, padx = 10)
+
+        # Create 'Save Key' button for saving the entered API key. When clicked, it calls the save_key() function
+        save_key_button = customtkinter.CTkButton(master = self.frame_key,
+                                                  text = 'Save',
+                                                  width = 68,
+                                                  height = 40,
+                                                  fg_color = '#5865f2',
+                                                  hover_color = '#2133ee',
+                                                  command = self.save_key)
+        save_key_button.grid(row = 0, column = 2, padx = 20)
+
+
+        # Start the tkinter event loop, which keeps the application running and handles user interactions
+        self.window.mainloop()
+
+
+    # Event function that sends the user's message to the bot and display chat history inside the chat window
+    def send_text_message(self, event):
+        # Get the text message from the user_msg_entry
+        user_message = self.user_msg_entry.get()
+        # Check if user's message is entered
+        if user_message.strip() != '':
+            # If the API key is not provided
+            if self.KEY == '':
+                # Display an error message that reminds the user to enter their API key
+                messagebox.showerror('API Key Not Provided', 'Please enter your API key to initiate your chat!')
+            # If the API key is provided
+            else:
+                # Enable text insertion into the chat window
+                self.chat_window.config(state = NORMAL)
+                # Insert the user's message into the chat window
+                self.chat_window.insert(END, 'You <{}>: '.format(datetime.now().strftime('%H:%M:%S %Y/%m/%d')) + user_message + '\n\n')
+                # Send the user's message to the bot for processing and retrieve the bot's response
+                self.bot_message = self.bot.chatting(user_message = user_message,
+                                                     model = self.MODEL)
+                # Insert the bot's message into the chat window
+                self.chat_window.insert(END, 'Bot <{}>: '.format(datetime.now().strftime('%H:%M:%S %Y/%m/%d')) + self.bot_message + '\n\n')
+                # Disable text insertion into the chat window
+                self.chat_window.config(state = DISABLED)
+                # Clear the user message entry
+                self.user_msg_entry.delete(0, END)
+
+
+    # Toggle the visibility of the API Key part
+    def toggle_key_part(self):
+        # Toggle the state of key_part_displayed between True and False
+        self.key_part_displayed = not self.key_part_displayed
+        # Check if key_part_displayed is True and display the API key part accordingly
+        if self.key_part_displayed:
+            # Display the API Key part
+            self.show_key_part()
+        else:
+            # Hide the API Key part
+            self.hide_key_part()
+
+
+    # Clear the chat history and reset bot_message
+    def clear_history(self):
+        # Enable text insertion into the chat window
+        self.chat_window.config(state = NORMAL)
+        # Reset bot_message to empty string
+        self.bot_message = ''
+        # Delete all messages in the chat window
+        self.chat_window.delete(1.0, END)
+        # Disable text insertion into the chat window
+        self.chat_window.config(state = DISABLED)
+
+
+    # Play the audio of the bot's message using the bot's text-to-speech functionality
+    def play_bot_message(self):
+        # If the API key is not provided...
+        if self.KEY == '':
+            # Display an error message to enter a valid API key
+            messagebox.showerror('API Key Not Provided', 'Please enter your API key to initiate your chat!')
+        else:
+            # If the bot's message is not empty
+            if self.bot_message.strip() != '':
+                # Play the audio of the bot's message
+                self.bot.saying(self.bot_message)
+
+
+    # Display the API key part of the application window
+    def show_key_part(self):
+        # Expand the application window vertically to display the API key part
+        self.window.geometry('720x740')
+        # Display the frame for the API key part
+        self.frame_key.pack(pady = 8)
+        # Set default value for API Key entry
+        self.key_entry.insert(0, self.KEY)
+
+
+    # Hide the API key part of the application window
+    def hide_key_part(self):
+        # Clear the API Key entry
+        self.key_entry.delete(0, END)
+        # Restore the application window back to its original size
+        self.window.geometry('720x615')
+        # Remove the frame for API key part from the app window
+        self.frame_key.pack_forget()
+
+
+    # Save the entered API key and initialize the chatbot with the new key
+    def save_key(self):
+        # Check if the API key entry is not empty
+        if self.key_entry.get().strip() != '':
+            # Check if the user has selected a valid GPT model
+            if self.model_var.get() == 'Select a GPT Model':
+                # Display an error message to remind selecting a GPT model
+                messagebox.showerror('GPT Model Not Selected', 'Please select a GPT model to initiate your chat!')
+            # If a valid GPT model is selected...
+            else:
+                # If the value in API key entry and selected GPT model both remain the same...
+                if (self.key_entry.get() == self.KEY) and (self.model_var.get() == self.MODEL):
+                    # Set key_part_displayed to False
+                    self.key_part_displayed = False
+                    # Hide the API key part
+                    self.hide_key_part()
+                # If the value in API key entry has been changed...
+                else:
+                    # Display a confirmation dialog to inform the user about the consequences of updating the API key
+                    ToSaveKey = messagebox.askyesno("Update API Key and GPT Model",
+                                                 """Updating the API key and/or GPT Model requires RESTARTING the ChatBot, which will clear the chat history. Are you sure you want to proceed?""")
+                    # If the user confirms to update the API key
+                    if ToSaveKey:
+                        # Update the API Key
+                        self.KEY = self.key_entry.get()
+                        # Update the selected GPT model
+                        self.MODEL = self.model_var.get()
+                        # Clear the chat history
+                        self.clear_history()
+                        # Enable text insertion into the chat window
+                        self.chat_window.config(state = NORMAL)
+                        # Insert a message into the chat window indicating that the API key is being updated
+                        self.chat_window.insert(END, "[Updating API Key...]\n\n")
+
+                        # Try start a new conversation with the chatbot
+                        try:
+                            # Instantiate the ChatBot with the new API key
+                            self.bot = ChatGPTBot(self.KEY)
+                            # Send a test message to the chatbot to ensure it is working
+                            self.bot_message = self.bot.chatting(user_message = 'hello',
+                                                                 model = self.MODEL)
+                            # If the chatbot is successfully loaded, display a success message in the chat window...
+                            self.chat_window.insert(END, "[Chatbot loaded successfully with the {} model! You may begin your conversation now.]\n\n".format(self.MODEL))
+                            # ...and the bot's response to the test message
+                            self.chat_window.insert(END, 'Bot <{}>: '.format(datetime.now().strftime('%H:%M:%S %Y/%m/%d')) + self.bot_message + "\n\n")
+                            # Set key_part_displayed to False
+                            self.key_part_displayed = False
+                            # Hide the API key part
+                            self.hide_key_part()
+                        # If the chatbot is failed to initialize
+                        except Exception as e:
+                            # Reinitialize the 3 variables
+                            self.bot = None
+                            self.KEY = ''
+                            self.bot_message = ''
+                            # Clear the API Key entry
+                            self.key_entry.delete(0, END)
+                            # Insert an error message into the chat window indicating the specific error encountered
+                            self.chat_window.insert(END, '[ERROR: Invalid API key provided]\n\n')
+                            # Set key_part_displayed to True
+                            self.key_part_displayed = True
+                            # Retain the API key part
+                            self.show_key_part()
+                            # Display an error message indicating the API key is invalid
+                            messagebox.showerror('Invalid API Key', 'API Key Invalid. Please ensure that a valid API key has been entered.')
+
+                        # Disable text insertion into the chat window
+                        self.chat_window.config(state = DISABLED)
+
+
+
+
+# Run the ChatBot App
+ChatBotApp = TkinterApp()
---
+++ README.md
@@ -0,0 +1,109 @@
+# ChatGPT Tkinter Desktop App
+
+[![GitHub](https://badgen.net/badge/icon/GitHub?icon=github&color=black&label)](https://github.com/MaxineXiong)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Made with Python](https://img.shields.io/badge/Python->=3.6-blue?logo=python&logoColor=white)](https://www.python.org)
+[![OpenAI API](https://img.shields.io/badge/OpenAI_API-E5E4E2?logo=OpenAI&logoColor=%23000000)](https://openai.com/blog/openai-api)
+[![ChatGPT](https://img.shields.io/badge/ChatGPT-00A67E?logo=openai)](https://chat.openai.com/)
+
+<br/>
+
+Welcome to the repository for the **unofficial ChatGPT Tkinter Desktop App**! This user-friendly application allows you to have natural language conversations
+with [ChatGPT](https://openai.com/index/chatgpt) directly from your local computer — provided you have internet access. Built upon OpenAI's [**Chat Completions API**](https://platform.openai.com/docs/guides/text-generation/chat-completions-api), it leverages advanced natural language processing capabilities to deliver high-quality responses to human's requests.
+Through an intuitive user interface, you can easily select between [***GPT-3.5 Turbo***](https://platform.openai.com/docs/models/gpt-3-5-turbo), [***GPT-4o***](https://platform.openai.com/docs/models/gpt-4o), [***GPT-4 Turbo***](https://platform.openai.com/docs/models/gpt-4-turbo-and-gpt-4) models, engage in conversations with ChatGPT, review previous interactions,
+and even listen to the chatbot's responses through audio playback. The goal of this application is to provide an immersive and enjoyable conversational experience that
+closely resembles interacting with a real person.
+
+***Press the CTRL key and click the badge below simultaneously to run the application** (Please be aware that the audio playback functionality may not be available when running the application on Replit)*:
+
+[![Run on Repl.it](https://replit.com/badge/github/MaxineXiong/ChatGPT-Tkinter-Desktop-App.git)](https://replit.com/@MaxineXiong/ChatGPT-Tkinter-Desktop-App?v=1)
+
+<br/>
+
+## Features
+
+- **Intuitive User Interface**: The **ChatGPT Tkinter Desktop App** provides a seamless and user-friendly interface, enabling effortless conversations with ChatGPT.
+- **Immersive Conversational Experience**: Engage in natural language conversations with ChatGPT using your own [OpenAI API key](https://platform.openai.com/api-keys), creating an authentic and immersive experience that closely resembles conversing with a real person.
+- **Model Selection**: Choose between the **GPT-3.5 Turbo**, **GPT-4o** and **GPT-4 Turbo** models to tailor the conversational capabilities to your specific needs, ensuring optimal performance and accuracy.
+- **Conversation History**: Easily access and review your past conversations within the app, allowing for convenient reference and continuity.
+- **Audio Playback**: Enhance your experience by listening to the chatbot's responses through built-in audio playback functionality, bringing the conversation to life.
+
+<br/>
+
+## Repository Structure
+
+The repository is structured as follows:
+```
+ChatGPT-Tkinter-Desktop-App/
+├── MAIN.py                   
+├── bot.py                   
+├── requirements.txt         
+├── icons/
+│   └── chat.ico
+├── virtual/
+│   └── *         
+├── README.md                 
+└── LICENSE                   
+```
+
+- **MAIN.py**: This file is the main entry point of the desktop app and contains the Python code responsible for handling the user interface and communication with the GPT model.
+- **bot.py**: This file contains the Python code that defines the class for the ChatGPTBot, encapsulating the functionality of the ChatGPT model and handling the generation of responses.
+- **requirements.txt**: This file lists all the required Python modules and packages necessary to run the desktop app. You can install these dependencies on your local computer by running the command `pip install -r requirements.txt`.
+- **icons/**: This folder contains the ICO image that is used as the app's window icon.
+- **virtual/**: This folder contains all files related to the virtual environment, facilitating an isolated space for Python package management.
+- **README.md**: Provides an overview of this repository.
+- **LICENSE**: The license file for the project.
+
+<br/>
+
+## Get Started
+
+To intall the desktop application on your computer, please follow these steps:
+
+1) Clone this repository to your local machine using the following command:
+    ```
+    git clone https://github.com/MaxineXiong/ChatGPT-Tkinter-Desktop-App.git
+    ```
+    Alternatively, you can download the EXE application from the web application **[Talk to GPT](https://maxinexiong-openai-api-web-apps-home-xbxlm8.streamlit.app/Talk_To_GPT)**.
+
+2) Download and install the latest version of [Python](https://www.python.org/downloads/) for your system. Make sure to select the "Add Python to PATH" option during the installation process.
+
+3) Navigate to the project folder using File Explorer, type `cmd` in the address bar at the top of the window, and press Enter. This will open Command Prompt in the project folder.
+
+4) Now launch the desktop application within the virtual environment by entering the following command in the Command Prompt:
+    ```
+    virtual\Scripts\python MAIN.py
+    ```
+
+
+Now you can start chatting with ChatGPT by entering your API key and selecting a GPT model, and then typing your messages in the input field and pressing Enter, as the video below demonstrates.
+
+[ChatGPT-Tkinter-Desktop-App-Demo](https://github.com/MaxineXiong/ChatGPT-Tkinter-Desktop-App/assets/55864839/089cf277-d42f-4d85-800f-7b75dbc39684)
+
+<br/>
+
+## Contributions
+
+Contributions to the **ChatGPT Tkinter Desktop App** are welcome! If you have any ideas, suggestions, or bug fixes, please open an issue or submit a pull request. Your contributions can help improve the functionality and user experience of the app.
+
+<br/>
+
+## License
+
+The **ChatGPT Tkinter Desktop App** is licensed under the [MIT License](https://choosealicense.com/licenses/mit/). Feel free to use, modify, and distribute the application in accordance with the terms of the license.
+
+<br/>
+
+## Acknowledgments
+
+I would like to express my gratitude to the open-source community for their invaluable contributions. The development of this app would not have been possible without the support and efforts of the following projects and individuals:
+
+- [OpenAI](https://openai.com/) for developing GPT models and APIs.
+- [Python](https://www.python.org/) for providing a powerful programming language.
+- [GitHub](https://github.com/) for hosting this repository and enabling collaboration.
+
+<br/>
+
+Thank you for using the **ChatGPT Tkinter Desktop App**! I hope you find it useful and enjoy your conversations with ChatGPT.
+
+
